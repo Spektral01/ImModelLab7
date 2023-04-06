727d87cda403272027f7c05f740181e8f9ae1498
Spektral01 <chalyhgrisha01@gmail.com>
2023-04-06 12:16:09 +0700

    t
    t

---
+++ ImModelLab7/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace ImModelLab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.DocEd = new System.Windows.Forms.NumericUpDown();
+            this.PatientEd = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.LifeEd = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DocEd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PatientEd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LifeEd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(735, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start/Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(60, 152);
+            this.chart1.Name = "chart1";
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series10.Legend = "Legend1";
+            series10.Name = "Здоровые люди";
+            series11.ChartArea = "ChartArea1";
+            series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series11.Legend = "Legend1";
+            series11.Name = "Вирусы";
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series12.Legend = "Legend1";
+            series12.Name = "Пациенты";
+            this.chart1.Series.Add(series10);
+            this.chart1.Series.Add(series11);
+            this.chart1.Series.Add(series12);
+            this.chart1.Size = new System.Drawing.Size(877, 495);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // DocEd
+            // 
+            this.DocEd.Location = new System.Drawing.Point(263, 32);
+            this.DocEd.Name = "DocEd";
+            this.DocEd.Size = new System.Drawing.Size(120, 22);
+            this.DocEd.TabIndex = 2;
+            this.DocEd.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // PatientEd
+            // 
+            this.PatientEd.Location = new System.Drawing.Point(263, 70);
+            this.PatientEd.Name = "PatientEd";
+            this.PatientEd.Size = new System.Drawing.Size(120, 22);
+            this.PatientEd.TabIndex = 3;
+            this.PatientEd.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Доля врачей";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Доля пациентов";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Уровень жизни";
+            // 
+            // LifeEd
+            // 
+            this.LifeEd.Location = new System.Drawing.Point(263, 110);
+            this.LifeEd.Name = "LifeEd";
+            this.LifeEd.Size = new System.Drawing.Size(120, 22);
+            this.LifeEd.TabIndex = 6;
+            this.LifeEd.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 659);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LifeEd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PatientEd);
+            this.Controls.Add(this.DocEd);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DocEd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PatientEd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LifeEd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.NumericUpDown DocEd;
+        private System.Windows.Forms.NumericUpDown PatientEd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown LifeEd;
+    }
+}
+
---
+++ ImModelLab7/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImModelLab7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public bool isStart=false;
+        public double Doctors, LifeQual, AllPatients;
+        public double  dt = 0.1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Doctors = (double)DocEd.Value;
+            LifeQual = (double)LifeEd.Value;
+            AllPatients = (double)PatientEd.Value;
+
+            if (!isStart)
+            {
+                isStart = true;
+                timer1.Start();
+            }
+            else
+            {
+                isStart = false;
+                timer1.Stop();
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public double viruses=10, factory=10, docInfect=5, patientInfect=15, healthyPerson=0, medicine=100, medicineForHospital=5, trash=0;
+        public int i = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            viruses = viruses + (factory - AllPatients)*1.6;
+            factory = factory+(LifeQual*0.2);
+            AllPatients = AllPatients + viruses - patientInfect; 
+            patientInfect = patientInfect + (AllPatients + Doctors - healthyPerson)*1.6;
+            Doctors = Doctors + LifeQual;
+            docInfect = Doctors * 0.5 + patientInfect * 2 + docInfect;
+            healthyPerson = healthyPerson + (patientInfect+medicineForHospital)*0.03;
+            medicine = medicine + factory * 1.2 - medicineForHospital * 0.5;
+            medicineForHospital = medicineForHospital + (medicine - trash) * 0.5;
+            trash = trash + medicineForHospital * 0.2;
+            chart1.Series[0].Points.AddXY(i, healthyPerson);
+            chart1.Series[1].Points.AddXY(i, viruses);
+            chart1.Series[2].Points.AddXY(i, patientInfect);
+            i++;
+
+        }
+    }
+}
